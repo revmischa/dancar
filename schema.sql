b1b72317ac3acf846ff103b032fd2d45490dd63c
Mischa S <revmischa@cpan.org>
2015-01-11 23:21:19 -0800

    keep track of when location was last updated

--- schema.sql
+++ schema.sql
@@ -3,6 +3,8 @@ CREATE EXTENSION postgis;
 DROP TABLE IF EXISTS "user";
 CREATE TABLE "user" (
   id SERIAL PRIMARY KEY,
+  created TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_location TIMESTAMP,
   name TEXT NOT NULL,
   location GEOGRAPHY
 );
@@ -10,6 +12,9 @@ CREATE TABLE "user" (
 CREATE OR REPLACE FUNCTION update_user_table() RETURNS TRIGGER AS $$
 DECLARE
 BEGIN
+IF ( (TG_OP = 'INSERT' AND NEW.location IS NOT NULL) OR (TG_OP = 'UPDATE' AND NEW.location IS DISTINCT FROM OLD.location) ) THEN
+	NEW.updated_location := current_timestamp;
+END IF;
 PERFORM pg_notify('user_updated', '{"id": ' 
     || CAST(NEW.id AS text) 
     || ', "location": '
@@ -23,7 +28,7 @@ $$ LANGUAGE plpgsql;
 --
 
 DROP TRIGGER IF EXISTS user_update_notify ON "user";
-CREATE TRIGGER user_update_notify AFTER UPDATE OR INSERT
+CREATE TRIGGER user_update_notify BEFORE UPDATE OR INSERT
     ON "user" FOR EACH ROW EXECUTE PROCEDURE update_user_table();
 
 INSERT INTO "user" (name) VALUES ('danh');
