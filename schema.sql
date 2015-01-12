c194f72073ac6ad9700a5bee2acbf4db5b3c7c82
Mischa S <revmischa@cpan.org>
2015-01-11 17:42:16 -0800

    rename device to user

--- schema.sql
+++ schema.sql
@@ -1,16 +1,16 @@
 CREATE EXTENSION postgis;
 
-DROP TABLE IF EXISTS device;
-CREATE TABLE device (
+DROP TABLE IF EXISTS "user";
+CREATE TABLE "user" (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   location GEOGRAPHY
 );
 
-CREATE OR REPLACE FUNCTION update_device_table() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION update_user_table() RETURNS TRIGGER AS $$
 DECLARE
 BEGIN
-PERFORM pg_notify('device_updated', '{"id": ' 
+PERFORM pg_notify('user_updated', '{"id": ' 
     || CAST(NEW.id AS text) 
     || ', "location": '
     || ST_AsGeoJSON(NEW.location)
@@ -22,8 +22,8 @@ $$ LANGUAGE plpgsql;
 
 --
 
-DROP TRIGGER IF EXISTS device_update_notify ON device;
-CREATE TRIGGER device_update_notify AFTER UPDATE OR INSERT
-    ON device FOR EACH ROW EXECUTE PROCEDURE update_device_table();
+DROP TRIGGER IF EXISTS user_update_notify ON "user";
+CREATE TRIGGER user_update_notify AFTER UPDATE OR INSERT
+    ON "user" FOR EACH ROW EXECUTE PROCEDURE update_user_table();
 
-INSERT INTO device (name) VALUES ('danh');
+INSERT INTO "user" (name) VALUES ('danh');
