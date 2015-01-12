597fe529305937240c7ac901f4aed3d30a30ec73
Mischa S <revmischa@cpan.org>
2015-01-11 22:32:07 -0800

    fixing user routes

--- dancar.py
+++ dancar.py
@@ -24,26 +24,26 @@ def close_db(error):
         g.pg_db.close()
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
 
 @app.route('/user/list')
-def list_users():
+def user_list():
     users=User.query.all()
     return render_template('users.html', users=users)
 
 @app.route('/user/<uid>')
-def view_user(uid):
-    user = User.query.get(did)
+def user_view(uid):
+    user = User.query.get(uid)
     if not user:
         abort(404)
     return render_template('map.html', user=user)
 
-@app.route('/user/<did>/update', methods=['POST'])
-def user_update(did):
+@app.route('/user/<uid>/update', methods=['POST'])
+def user_update(uid):
     lng = request.form['lng']
     lat = request.form['lat']
-    user = User.query.get(did)
+    user = User.query.get(uid)
     if not user:
         return "Does not exist"
     user.set_location(lng, lat)
