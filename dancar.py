bcbc876286efa99e81a2fbc72c426123ce157da1
Mischa S <revmischa@cpan.org>
2015-01-12 01:25:35 -0800

    import cleanup

--- dancar.py
+++ dancar.py
@@ -1,6 +1,4 @@
-from flask import Flask
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash, jsonify
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
 import os
 from models import db, User
 from flask_sockets import Sockets
