b131adf722ee66bc568a8a9384982fe5c9b26012
Ryan Yoosefi <ryan.yoosefi@gmail.com>
2015-01-14 01:41:07 -0800

    models :: removed unused imports

--- models.py
+++ models.py
@@ -1,13 +1,11 @@
-from flask.ext.sqlalchemy import SQLAlchemy
 from geoalchemy2 import Geography
-from geoalchemy2.functions import ST_X, ST_Y
 from geoalchemy2.shape import to_shape
-from sqlalchemy.ext.declarative import declarative_base
 from dancar import db
 
 class User(db.Model):
-    __tablename__ = 'user'
 
+    # DB Setup
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
     updated_location = db.Column(db.DateTime())
@@ -24,9 +22,7 @@ class User(db.Model):
         self.location = "POINT(%0.16f %0.16f)" % (float(lng), float(lat))
 
     def get_lng(self):
-        point = to_shape(self.location)
-        return point.x
+        return to_shape(self.location).x
+
     def get_lat(self):
-        point = to_shape(self.location)
-        return point.y
-        
+        return to_shape(self.location).y
