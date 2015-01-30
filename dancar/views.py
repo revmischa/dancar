17c03c9076f983da836decfa8e4e8a0d1f702e75
Mischa S <revmischa@cpan.org>
2015-01-29 21:01:28 -0800

    comments for routes, view self route

--- dancar/views.py
+++ dancar/views.py
@@ -6,27 +6,38 @@ from .models import User
 from flask import render_template as render, jsonify, request
 from flask_user import current_user, login_required
 
-
+# home
 @app.route('/')
 def index():
     return render('index.html')
 
+# view all users
 @app.route('/user/list')
 @login_required
 def user_list():
     return render('users.html', users=User.query.all())
 
+# view a user's status
+# (should have some security on this)
 @app.route('/user/<uid>')
 @login_required
 def user_view(uid):
     return render('map.html',user=User.query.get(uid))
 
+# view myself
+@app.route('/user/me')
+@login_required
+def user_view_me():
+    return render('map.html',user=current_user)
+
+# update my position
 @app.route('/api/user/update', methods=['POST'])
 @login_required
 def user_update():
     current_user.set_location(request.form['lng'],request.form['lat'])
     return "Location updated."
 
+# get my user info
 @app.route('/api/user/info', methods=['GET'])
 @login_required
 def api_user():
