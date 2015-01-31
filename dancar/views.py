430531ba93b434d48ede18fc7b6aec44752d39ba
danhimalplanet <danhimalplanet@gmail.com>
2015-01-31 13:11:23 -0800

    got rid of bcrypt.  use user_manager.verify_password instead.  cant find verify_password for some unknown reason

--- dancar/views.py
+++ dancar/views.py
@@ -1,7 +1,6 @@
 # Importing this module will bind routes to the app.
 # This could be futher split up into submodules if the number of endpoints grows too large for one file.
 
-import bcrypt
 from . import app
 from .models import User
 from flask import abort, jsonify, request
@@ -59,11 +58,8 @@ def workspace_api_login():
 
     user, user_email = app.user_manager.find_user_by_email(email)
 
-    # If email is valid
     if user and user.active:
-        # If password matches password.hash in user
-        # NOTE: This should probably be moved to models
-        if bcrypt.hashpw(password, user.password) == user.password:
+        if user.verify_password(password, user):
             return "Valid email and password"
         else:
             return "Invalid password", 403
