e0d4651f7f1d3466ab53ea0d47ca996038413ff7
Mischa S <revmischa@cpan.org>
2015-02-04 23:22:53 -0800

    osx db config (sorry nick), travis config

--- dancar/config.py
+++ dancar/config.py
@@ -6,7 +6,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.getenv('SECRET_KEY', 'THIS IS AN INSECURE SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql:///dancar')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://localhost/dancar')
     USER_LOGIN_TEMPLATE = 'flask_user/login_or_register.html'
     USER_REGISTER_TEMPLATE = 'flask_user/login_or_register.html'
     USER_ENABLE_USERNAME = False
