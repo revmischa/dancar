dc507b1afed1e1f21bf31f35ba1b98544b42f8a5
Mischa Spiegelmock <revmischa@cpan.org>
2016-01-25 17:40:37 -0800

    heroku new db

--- dancar/config.py
+++ dancar/config.py
@@ -7,7 +7,7 @@ class Config(object):
     CSRF_ENABLED = True
     CORS_ENABLED = True
     SECRET_KEY = os.getenv('SECRET_KEY', 'THIS IS AN INSECURE SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://localhost/dancar')
+    SQLALCHEMY_DATABASE_URI = os.getenv('HEROKU_POSTGRESQL_ONYX_URL', 'postgresql://localhost/dancar')
     USER_LOGIN_TEMPLATE = 'flask_user/login_or_register.html'
     USER_REGISTER_TEMPLATE = 'flask_user/login_or_register.html'
     USER_ENABLE_USERNAME = False
